a0f17d9e0593d8b14235d8a816a6808788f6a758
AerideS <diturea@gmai.com>
2024-04-21 21:26:14 +0900

    rest api 활용해서 드론 데이터 가져옴

--- db/db.py
+++ db/db.py
@@ -1,16 +1,18 @@
 from pymongo import MongoClient 
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 
 # MongoDB connection
 client = MongoClient('mongodb://203.255.57.122:27018/')
 db = client['drone_data1']
 collection = db['drone_data']
 
-# Flask app setup
+# Flask app setupls
 app = Flask(__name__)
+CORS(app)  # 모든 엔드포인트에 CORS를 활성화합니다.
 
 # Route to fetch data from MongoDB and render it in an HTML template
-@app.route("/")  
+@app.route("/", methods=['GET', 'POST'])  
 def root():
     data = collection.find_one({'drone_id': 'drone1'})
     data_copy = data.copy()
@@ -19,4 +21,5 @@ def root():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port="8080", debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
+
