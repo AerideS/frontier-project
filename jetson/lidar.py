7dc6b9bbab233adcd9026a9bfd1b2ff5501eaca3
ksdfs1 <mbjo001101@gmail.com>
2024-05-16 17:16:52 +0900

    라이다 모듈 완성

--- jetson/lidar.py
+++ jetson/lidar.py
@@ -2,52 +2,50 @@ import serial
 import Jetson.GPIO as GPIO
 import time
 
-# UART 포트 설정
-serial_port = '/dev/ttyTHS1'
-# serial_port = '/dev/ttyS0'
-baud_rate = 115200
+class LidarModule:
+    def __init__(self) -> None:
+        # UART 포트 설정
+        self.serial_port = '/dev/ttyTHS1'
+        self.baud_rate = 115200
+        self.HEADER = 89  # (89 = 0x59)
+        self.DATA_LENGTH = 9
+        # UART 초기화
+        self.uart = serial.Serial(self.serial_port, self.baud_rate, timeout=1)
 
-# GPIO 핀 설정
-TX_pin = 33  # GPIO 핀 번호 설정
-RX_pin = 19 # GPIO 핀 번호 설정
+    def getAltitude(self):
+        try:
+            buf = [_ for _ in range(9)]
+            while True:
+                # 시리얼 포트로부터 읽기
+                data = self.uart.read()
+                if data:
+                    if int(data[0]) == self.HEADER:
+                        buf[0] = self.HEADER
+                        if int(self.uart.read()[0]) == self.HEADER:
+                            buf[1] = self.HEADER
 
-HEADER = 89
-DATA_LENGTH = 9
+                            for i in range(2, 9):
+                                buf[i] = int(self.uart.read()[0])
 
-# UART 초기화
-uart = serial.Serial(serial_port, baud_rate, timeout=1)
-buf = [_ for _ in range(9)]
-try:
-    # GPIO 초기화
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(TX_pin, GPIO.OUT)
-    GPIO.setup(RX_pin, GPIO.IN)
-    
-    while True:
-        # 시리얼 포트로부터 읽기
-        data = uart.read()
-        if data:
-            if int(data[0]) == HEADER:
-                buf[0] = HEADER
-                if int(uart.read()[0]) == HEADER:
-                    buf[1] = HEADER
+                            check = sum(buf[:8])
+                            if buf[8] == (check & 255):
+                                # 거리 및 신호 강도 계산
+                                dist = buf[2] + buf[3] * 256
+                                strength = buf[4] + buf[5] * 256
 
-                    for i in range(2, 9):
-                        buf[i] = int(uart.read()[0])
+                                # 거리 및 신호 강도 출력
+                                print("dist =", dist, "\t strength =", strength)
 
-                    check = sum(buf[:8])
-                    if buf[8] == (check & 255):
-                        # 거리 및 신호 강도 계산
-                        dist = buf[2] + buf[3] * 256
-                        strength = buf[4] + buf[5] * 256
-                        
-                        # 거리 및 신호 강도 출력
-                        print("dist =", dist, "\t strength =", strength)
-                
-except Exception as e:
-    print("Error:", str(e))
+                                return dist
 
-finally:
-    # 리소스 해제
-    uart.close()
-    GPIO.cleanup()
+        except Exception as e:
+            print("Error:", str(e))
+
+        finally:
+            # 리소스 해제
+            self.uart.close()
+
+if __name__ == '__main__':
+    lidar_module = LidarModule()
+    distance = lidar_module.getAltitude()
+    print(distance)
